a88e65457c1c97951056ee0ef3fb1ea86dd03039
s1f102100704 <s1f102100704@iniad.org>
2024-07-14 01:08:02 +0900

    交通手段の追加

--- client/pages/travelDestination/index.page.tsx
+++ client/pages/travelDestination/index.page.tsx
@@ -17,13 +17,26 @@ const TravelDestination = () => {
   return (
     <div className={styles.container}>
       <h1>TravelDestination</h1>
-      <div className={styles.departureBox}>
-        <div className={styles.depart}>出発</div>
-        <input />
+      <div className={styles.box}>
+        <div className={styles.subject}>交通手段</div>
+        <select className={styles.sel}>
+          <option>電車</option>
+          <option>自動車</option>
+          <option>徒歩</option>
+        </select>
       </div>
-      <div className={styles.arrivalBox}>
-        <div className={styles.depart}>到着</div>
-        <input value={destination} onChange={(e) => setDestination(e.target.value)} />
+
+      <div className={styles.box}>
+        <div className={styles.subject}>出発地</div>
+        <input className={styles.inp} />
+      </div>
+      <div className={styles.box}>
+        <div className={styles.subject}>目的地</div>
+        <input
+          value={destination}
+          onChange={(e) => setDestination(e.target.value)}
+          className={styles.inp}
+        />
       </div>
       <button onClick={serch} className={styles.serch}>
         検索
