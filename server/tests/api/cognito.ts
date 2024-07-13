a970f56b73c8ef34190b75a54da7d5382e9a0258
imoken777 <s1f102301640@iniad.org>
2024-07-13 18:53:38 +0900

    テスト削除

--- server/tests/api/cognito.ts
+++
@@ -1,8 +0,0 @@
-import { CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider';
-import { COGNITO_POOL_ENDPOINT } from 'service/envValues';
-
-export const cognitoClient = new CognitoIdentityProviderClient({
-  endpoint: COGNITO_POOL_ENDPOINT,
-  region: 'ap-northeast-1',
-  credentials: { accessKeyId: 'cognito-access-key', secretAccessKey: 'cognito-secret-key' },
-});
