85f0398298af8e56b68037fde256982afc0f55a1
s1f10210254 <s1f102102548@iniad.org>
2024-07-16 03:50:37 +0900

    スクレイピングを行い、観光地名、概要、住所を取得

--- client/pages/travelDestination/index.page.tsx
+++ client/pages/travelDestination/index.page.tsx
@@ -1,14 +1,15 @@
+import type { TravelSpot } from 'common/types/travelSpots';
 import { useState } from 'react';
 import { apiClient } from 'utils/apiClient';
 
 const TravelDestination = () => {
   const [searchQuery, setSearchQuery] = useState<string>('');
-  const [travelSpots, setTravelSpots] = useState<string[]>([]);
+  const [travelSpot, setTravelSpot] = useState<TravelSpot[]>([]);
 
   const fetchTravelSpots = async () => {
     console.log('searchQuery', searchQuery);
     await apiClient.travelStartingSpot.$post({ body: { destination: searchQuery } }).then((res) => {
-      setTravelSpots(res);
+      setTravelSpot(res);
     });
   };
   return (
@@ -17,8 +18,13 @@ const TravelDestination = () => {
       <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
       <button onClick={fetchTravelSpots}>検索</button>
       <ul>
-        {travelSpots.map((spot, index) => (
-          <li key={index}>{spot}</li>
+        {travelSpot.map((spot, index) => (
+          <li key={index} style={{ borderBottom: '1px solid #ccc', padding: '20px 0' }}>
+            <h2 style={{ margin: '0', fontSize: '1.5em' }}>名前：{spot.name}</h2>
+            <p style={{ margin: '5px 0', color: '#555' }}>概要：{spot.description}</p>
+            <br />
+            <p style={{ margin: '5px 0', color: '#555' }}>住所:{spot.address}</p>
+          </li>
         ))}
       </ul>
     </div>
