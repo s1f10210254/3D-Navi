91b62cb6d7f93a5b2477ff5ffe9de19aa54f6789
imoken777 <s1f102301640@iniad.org>
2024-07-17 01:23:12 +0900

    命名とスタイル方法整理

--- client/pages/travelDestination/index.page.tsx
+++ client/pages/travelDestination/index.page.tsx
@@ -5,24 +5,24 @@ import { apiClient } from 'utils/apiClient';
 
 const TravelDestination = () => {
   const imageUrl = '';
-  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [userDestination, setUserDestination] = useState<string>('');
   const [travelSpots, setTravelSpots] = useState<TravelSpot[]>([]);
 
   const fetchTravelSpots = async () => {
-    console.log('searchQuery', searchQuery);
-    await apiClient.travelStartingSpot.$post({ body: { destination: searchQuery } }).then((res) => {
-      setTravelSpots(res);
+    const res = await apiClient.travelStartingSpot.$post({
+      body: { destination: userDestination },
     });
+    setTravelSpots(res);
   };
 
   return (
     <div className={styles.container}>
-      <h1 style={{ fontSize: '50px', textAlign: 'center', width: '600px', height: '200px' }}>
+      <h1 className={styles.title}>
         TravelDestination
         <br />
       </h1>
       {/* ここになにかしらのアイコン画像を入れいる */}
-      <img src={imageUrl} alt="Google Image" style={{ width: '300px', height: 'auto' }} />
+      <img src={imageUrl} alt="Google Image" className={styles.image} />
       <p>行きたい場所を入力してください</p>
       <div className={styles.box}>
         <div className={styles.subject}>移動手段</div>
@@ -40,23 +40,23 @@ const TravelDestination = () => {
       <div className={styles.box}>
         <div className={styles.subject}>観光地</div>
         <input
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
+          value={userDestination}
+          onChange={(e) => setUserDestination(e.target.value)}
           className={styles.inp}
           placeholder="例:京都"
         />
       </div>
-      <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+      <input value={userDestination} onChange={(e) => setUserDestination(e.target.value)} />
       <button onClick={fetchTravelSpots} className={styles.serch}>
         検索
       </button>
       <ul>
         {travelSpots.map((spot, index) => (
-          <li key={index} style={{ borderBottom: '1px solid #ccc', padding: '20px 0' }}>
-            <h2 style={{ margin: '0', fontSize: '1.5em' }}>名前：{spot.name}</h2>
-            <p style={{ margin: '5px 0', color: '#555' }}>概要：{spot.description}</p>
+          <li key={index} className={styles.listItem}>
+            <h2 className={styles.listTitle}>名前：{spot.name}</h2>
+            <p className={styles.listDescription}>概要：{spot.description}</p>
             <br />
-            <p style={{ margin: '5px 0', color: '#555' }}>
+            <p className={styles.listDescription}>
               緯度:{spot.location.latitude}、経度:{spot.location.longitude}
             </p>
           </li>
