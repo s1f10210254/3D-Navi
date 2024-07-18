d6527a534a31b966d939414a21b4cc305ec8db32
s1f102100704 <s1f102100704@iniad.org>
2024-07-18 21:15:38 +0900

    headerにgithubのicon link追加

--- client/components/header/Header.tsx
+++ client/components/header/Header.tsx
@@ -1,9 +1,18 @@
 import styles from './header.module.css';
+import iconLogo from './img/github-logo_icon-icons.webp';
 
 export const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.appTitle}>Travel</div>
+      <a
+        href="https://github.com/imoken777/hackathon-vol9"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.logo}
+      >
+        <img src={iconLogo.src} className={styles.githubLogo} alt="logo" />
+      </a>
     </div>
   );
 };
