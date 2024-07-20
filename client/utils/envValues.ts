e3579af98a4e9dbfab9948ab7de82b59182d6191
imoken777 <s1f102301640@iniad.org>
2024-07-20 19:48:03 +0900

    envの扱い方を修正

--- client/utils/envValues.ts
+++ client/utils/envValues.ts
@@ -4,8 +4,6 @@ const NEXT_PUBLIC_API_BASE_PATH = z.string().parse(process.env.NEXT_PUBLIC_API_B
 
 const NEXT_PUBLIC_SERVER_PORT = z.string().optional().parse(process.env.NEXT_PUBLIC_SERVER_PORT);
 
+const NEXT_PUBLIC_MAPBOX_API_KEY = z.string().parse(process.env.NEXT_PUBLIC_MAPBOX_API_KEY);
 
-export {
-  NEXT_PUBLIC_API_BASE_PATH,
-  NEXT_PUBLIC_SERVER_PORT,
-};
+export { NEXT_PUBLIC_API_BASE_PATH, NEXT_PUBLIC_MAPBOX_API_KEY, NEXT_PUBLIC_SERVER_PORT };
